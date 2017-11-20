1fbf828277b63c3565c799bbc1cc6f27e850817d
bm295 <baominh.nguyen.295@gmail.com>
2017-11-20 22:43:11 +0700

    first commit

---
+++ WebSiteRoute/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebSiteRoute.Global" Language="C#" %>
---
+++ WebSiteRoute/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+
+namespace WebSiteRoute
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.MapPageRoute("Home", "Home/Index", "~/Default.aspx");
+            RouteTable.Routes.MapPageRoute("About", "Home/About", "~/About.aspx");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
