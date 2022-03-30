0a6fb2b0e8671e71a582c2e3c65fd7c8e4a161d1
Minh <baominh.nguyen.295@gmail.com>
2022-03-30 23:01:45 +0700

    add InfluxDB token and InfluxDBService

---
+++ WebSiteRoute/Services/InfluxDbService.cs
@@ -0,0 +1,28 @@
+﻿using InfluxDB.Client;
+
+namespace WebSiteRoute.Services
+{
+    public class InfluxDbService
+    {
+        private readonly string _token;
+
+        public InfluxDbService(IConfiguration configuration)
+        {
+            _token = configuration.GetValue<string>("InfluxDb:Token");
+        }
+
+        public void Write(Action<WriteApi> action)
+        { 
+            using var client = InfluxDBClientFactory.Create("https://51h585.stackhero-network.com", _token);
+            using var write = client.GetWriteApi();
+            action(write);
+        }
+
+        public async Task<T> QueryAsync<T>(Func<QueryApi, Task<T>> action)
+        {
+            using var client = InfluxDBClientFactory.Create("https://51h585.stackhero-network.com", _token);
+            var query = client.GetQueryApi();
+            return await action(query);
+        }
+    }
+}
