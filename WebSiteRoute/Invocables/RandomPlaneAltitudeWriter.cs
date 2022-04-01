754ba0f256752301026ec4c274a26942f51057f6
Minh <baominh.nguyen.295@gmail.com>
2022-04-01 23:12:33 +0700

    add Coravel and Invocable

---
+++ WebSiteRoute/Invocables/RandomPlaneAltitudeWriter.cs
@@ -0,0 +1,12 @@
+﻿using Coravel.Invocable;
+
+namespace WebSiteRoute.Invocables
+{
+    public class RandomPlaneAltitudeWriter : IInvocable
+    {
+        public Task Invoke()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebSiteRoute/Program.cs
+++ WebSiteRoute/Program.cs
@@ -1,7 +1,10 @@
+using WebSiteRoute.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<InfluxDbService>();
 
 var app = builder.Build();
 
